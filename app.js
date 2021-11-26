1fafbeb3775ee05fe1ab87eeae06978f818698a4
brianMunyao <brianmunyao6@gmail.com>
2021-11-26 22:01:02 +0300

    minor

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
-app.user(cors());
+app.use(cors());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
